b9b689ec213aa3327c582fca6407b2179ec9834c
siid sharma <siid123ck@gmail.com>
2023-08-24 23:04:59 +1000

    initialized the project

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className="hero_title">
+                Find, book or rent a car easily and quickly
+            </h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
