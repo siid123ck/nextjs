3514ee2acc7d58291bf0f027a2efa1d00f24045d
siid sharma <siid123ck@gmail.com>
2023-08-26 23:52:47 +1000

    layout and navigation

---
+++ types/index.ts
@@ -0,0 +1,16 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title:string;
+    containerStyles?:string;
+    btnType?:"button"|"submit";
+    handleClick?:MouseEventHandler<HTMLButtonElement>;
+}
+export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+  }
+export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManufacturer: (manufacturer: string) => void;
+}
